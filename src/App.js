f1b7161f576cda806400d4de4d654115a2482202
Rainacam <rainacam12@gmail.com>
2023-06-16 21:22:10 -0400

    wave-2 complete

--- src/App.js
+++ src/App.js
@@ -12,17 +12,36 @@ const TASKS = [
     id: 2,
     title: 'Cook Pasta',
     isComplete: true,
+  
   },
 ];
 
 const App = () => {
+  const [tasks, setTasks] = React.useState(TASKS)
+
+  const deleteTask = (id) => {
+    setTasks(prevTasks => {
+      const updatedTask = prevTasks.filter(task => task.id !== id)
+      return updatedTask
+    })
+  }
+  const toggleTask = (id) => {
+    setTasks(prevTasks => {
+      const updatedTask = prevTasks.map(task => {
+        return task.id === id ? { ...task, isComplete: !task.isComplete} : task
+      })
+      return updatedTask
+    })
+    }
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask}/>}</div>
       </main>
     </div>
   );
